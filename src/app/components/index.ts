091e35b693ec7f5eb776987e555337e5064cbafb
SaadElQuaul <saadelquaul@gmail.com>
2026-01-19 10:47:34 +0100

    add index file to export components for home, track, album, category, and audio player

---
+++ src/app/components/index.ts
@@ -0,0 +1,17 @@
+// Home component
+export * from './home/home.component';
+
+// Track components
+export * from './track-list/track-list.component';
+export * from './track-form/track-form.component';
+
+// Album components
+export * from './album-list/album-list.component';
+export * from './album-form/album-form.component';
+export * from './album-detail/album-detail.component';
+
+// Category components
+export * from './category-list/category-list.component';
+
+// Audio Player
+export * from './audio-player/audio-player.component';
